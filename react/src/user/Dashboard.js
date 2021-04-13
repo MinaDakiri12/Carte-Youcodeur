70d74d1f7ecc2d370cc76d594785e62279db1e43
MinaDakiri <minadakiri7@gmail.com>
2021-04-13 11:24:24 +0000

    Dashboard of User

--- react/src/user/Dashboard.js
+++ react/src/user/Dashboard.js
@@ -1,9 +1,13 @@
 import React, {Fragment} from 'react'
-
 import Layout from './../core/Layout'
+import {isAuthenticated} from './../auth/helpers'
 
 function Dashboard() {
+
+    const {user:{first_name, last_name, email, role}} = isAuthenticated()
+
     return (
+
        <Fragment>
            <Layout
            title= "Dashboard"
@@ -11,6 +15,33 @@ function Dashboard() {
            className="container"
 
            >
+                <div className="row">
+                    <div className="col-md-6">
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">User Information</h5>
+                                <ul className="list-group list-group-flush">
+                                    <li className="list-group-item">{first_name}</li>
+                                    <li className="list-group-item">{last_name}</li>
+                                    <li className="list-group-item">{email}</li>
+                                    <li className="list-group-item">{role? 'admin' : 'user'}</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-6">
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title"></h5>
+                                <ul className="list-group list-group-flush">
+                                    <li className="list-group-item"></li>
+                                    <li className="list-group-item"></li>
+                                    <li className="list-group-item"></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                
            </Layout>
        </Fragment>
